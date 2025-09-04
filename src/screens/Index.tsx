6c9266523142e770578d94391b87050a613634e5
Cristian Prochnow <cristianprochnow@gmail.com>
2025-09-03 23:33:16 -0300

    feat: add footer menu

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -1,5 +1,5 @@
-import { Bell } from "lucide-react"
-import { Outlet } from "react-router"
+import { Bell, Blocks, Inbox, LayoutTemplate, Workflow } from "lucide-react"
+import { Link, Outlet } from "react-router"
 import '../styles/screens/index.css'
 
 export function Index() {
@@ -23,7 +23,46 @@ export function Index() {
                 <Outlet />
             </main>
 
-            <footer className="wrapper"></footer>
+            <footer>
+                <nav>
+                    <Link to="/templates" className="menu-item active">
+                        <div className="icon-container">
+                            <span className="icon">
+                                <LayoutTemplate />
+                            </span>
+                            <span className="description">Templates</span>
+                        </div>
+                        <span className="label">Templates</span>
+                    </Link>
+                    <Link to="/templates" className="menu-item">
+                        <div className="icon-container">
+                            <span className="icon">
+                                <Workflow />
+                            </span>
+                            <span className="description">Integrações</span>
+                        </div>
+                        <span className="label">Integrações</span>
+                    </Link>
+                    <Link to="/templates" className="menu-item">
+                        <div className="icon-container">
+                            <span className="icon">
+                                <Blocks />
+                            </span>
+                            <span className="description">Perfis</span>
+                        </div>
+                        <span className="label">Perfis</span>
+                    </Link>
+                    <Link to="/templates" className="menu-item">
+                        <div className="icon-container">
+                            <span className="icon">
+                                <Inbox />
+                            </span>
+                            <span className="description">Publicações</span>
+                        </div>
+                        <span className="label">Publicações</span>
+                    </Link>
+                </nav>
+            </footer>
         </div>
     )
 }
