441dad9fe9011f1b5d311319752c7210674a6b7c
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-02 21:29:19 -0300

    feat(screens): add dinamic loading for selects into post creation

--- src/screens/Posts/Add.tsx
+++ src/screens/Posts/Add.tsx
@@ -1,59 +1,149 @@
-import { ArrowLeft, Save } from 'lucide-react'
-import { useNavigate } from 'react-router'
-import { ActionButton } from '../../components/ActionButton'
-import { OutlineButton } from '../../components/OutlineButton'
-import { PageTitle } from '../../components/PageTitle'
-import '../../styles/screens/posts/add.css'
+import { ArrowLeft, Save } from 'lucide-react';
+import { useNavigate } from 'react-router';
+import { ActionButton } from '../../components/ActionButton';
+import { OutlineButton } from '../../components/OutlineButton';
+import { PageTitle } from '../../components/PageTitle';
+import '../../styles/screens/posts/add.css';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+type TemplateOption = {
+  template_id: number
+  template_name: string
+};
+
+type FetchTemplateListResponse = {
+  resource: {
+    ok: boolean
+    error: string
+  }
+  templates: TemplateOption[]
+};
+
+type IntProfileOption = {
+  int_profile_id: number
+  int_profile_name: string
+  color_hex: string
+};
+
+type FetchIntProfileListResponse = {
+  resource: {
+    ok: boolean
+    error: string
+  }
+  int_profiles: IntProfileOption[]
+};
 
 export function Add() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [templateOptions, setTemplateOptions] = useState<TemplateOption[]>([]);
+  const [intProfileOptions, setIntProfileOptions] = useState<IntProfileOption[]>([]);
+  const [isLoading, setLoading] = useState(false);
 
-    function goBack() {
-        navigate(-1)
+  useEffect(() => {
+    loadDropdowns()
+  }, []);
+
+  function goBack() {
+    navigate(-1);
+  }
+
+  async function loadDropdowns() {
+    setLoading(true);
+
+    await loadTemplates();
+    await loadIntProfiles();
+
+    setLoading(false);
+  }
+
+  async function loadTemplates() {
+    const url = import.meta.env.VITE_GATEWAY_ENDPOINT;
+
+    const response = await fetch(url + '/templates/basic');
+    const data = await response.json() as FetchTemplateListResponse;
+
+    const hasData = data.templates && data.templates.length > 1;
+
+    if (!data.resource.ok) {
+      toast.error('Erro durante a busca de templates: ' +data.resource.error);
     }
 
-    return (
-        <div id="screen-post-add">
-            <header>
-                <OutlineButton label="Voltar" Icon={ArrowLeft} onClick={goBack} />
-                <PageTitle>Nova publicação</PageTitle>
-            </header>
+    if (!hasData) {
+      toast.info('Nenhum template encontrado :(');
+    }
 
-            <form className="form-add-container">
-                <div className="field-group">
-                    <label htmlFor="post_name">Apelido</label>
-                    <input type="text" name="post_name" id="post_name" />
-                </div>
+    setTemplateOptions(hasData ? data.templates : []);
+  }
 
-                <div className="col-2">
-                    <div className="field-group">
-                        <label htmlFor="template_id">Template</label>
-                        <select name="template_id" id="template_id" defaultValue="">
-                            <option value="" disabled>Selecione</option>
-                            <option value="1">Template 1</option>
-                            <option value="2">Template 2</option>
-                        </select>
-                    </div>
+  async function loadIntProfiles() {
+    const url = import.meta.env.VITE_GATEWAY_ENDPOINT;
 
-                    <div className="field-group">
-                        <label htmlFor="int_profile_id">Perfil</label>
-                        <select name="int_profile_id" id="int_profile_id" defaultValue="">
-                            <option value="" disabled>Selecione</option>
-                            <option value="1">Perfil 1</option>
-                            <option value="2">Perfil 2</option>
-                        </select>
-                    </div>
-                </div>
+    const response = await fetch(url + '/int_profiles/basic');
+    const data = await response.json() as FetchIntProfileListResponse;
 
-                <div className="field-group">
-                    <label htmlFor="post_content">Conteúdo</label>
-                    <textarea name="post_content" id="post_content"></textarea>
-                </div>
-            </form>
+    const hasData = data.int_profiles && data.int_profiles.length > 1;
 
-            <footer>
-                <ActionButton label="Salvar" Icon={Save} />
-            </footer>
+    if (!data.resource.ok) {
+      toast.error('Erro durante a busca de perfis: ' + data.resource.error);
+    }
+
+    if (!hasData) {
+      toast.info('Nenhum perfil de integração encontrado :(');
+    }
+
+    setIntProfileOptions(hasData ? data.int_profiles : []);
+  }
+
+  return (
+    <div id="screen-post-add">
+      <header>
+        <OutlineButton label="Voltar" Icon={ArrowLeft} onClick={goBack}/>
+        <PageTitle>Nova publicação</PageTitle>
+      </header>
+
+      <form className="form-add-container">
+        <div className="field-group">
+          <label htmlFor="post_name">Apelido</label>
+          <input type="text" name="post_name" id="post_name"/>
         </div>
-    )
+
+        <div className="col-2">
+          <div className={`field-group ${isLoading ? 'disabled' : ''}`}>
+            <div className={`loader ${!isLoading ? 'hidden' : ''}`}></div>
+
+            <label htmlFor="template_id">Template</label>
+            <select name="template_id" id="template_id" defaultValue="" disabled={isLoading}>
+              <option value="" disabled>Selecione</option>
+              {templateOptions.map(({ template_id, template_name }) => (
+                <option value={template_id}>{template_name}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className={`field-group ${isLoading ? 'disabled' : ''}`}>
+            <div className={`loader ${!isLoading ? 'hidden' : ''}`}></div>
+            <label htmlFor="int_profile_id">Perfil</label>
+            <select name="int_profile_id" id="int_profile_id" defaultValue="" disabled={isLoading}>
+              <option value="" disabled>Selecione</option>
+              {intProfileOptions.map(({ int_profile_id, int_profile_name, color_hex }) => (
+                <option style={{ color: `#${color_hex}` }} value={int_profile_id}>
+                  {int_profile_name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className="field-group">
+          <label htmlFor="post_content">Conteúdo</label>
+          <textarea name="post_content" id="post_content"></textarea>
+        </div>
+      </form>
+
+      <footer>
+        <ActionButton label="Salvar" Icon={Save}/>
+      </footer>
+    </div>
+  );
 }
