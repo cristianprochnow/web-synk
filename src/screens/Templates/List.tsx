ddf674f7ea445098f44f570250a5faa6edb4045a
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-15 18:26:05 -0300

    feat(screens): add header into template list screen

--- src/screens/Templates/List.tsx
+++ src/screens/Templates/List.tsx
@@ -1,9 +1,23 @@
 import '../../styles/screens/templates/list.css';
+import { PageTitle } from '../../components/PageTitle.tsx';
+import { ActionButton } from '../../components/ActionButton.tsx';
+import { Plus } from 'lucide-react';
+import { useNavigate } from 'react-router';
 
 export function List() {
+  const navigate = useNavigate();
+
+  function goToAdd() {
+    navigate('/templates/add');
+  }
+
   return (
     <div id="screen-template-list">
-      template list
+      <header>
+        <PageTitle>Templates</PageTitle>
+
+        <ActionButton label="Novo template" Icon={Plus} onClick={goToAdd}/>
+      </header>
     </div>
   );
 }
