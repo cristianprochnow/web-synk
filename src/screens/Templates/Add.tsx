485ebe7f3c3faa38ccaa9baf907f663c80fae4b1
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-16 23:39:43 -0300

    feat(screens): add base layout for template add

--- src/screens/Templates/Add.tsx
+++ src/screens/Templates/Add.tsx
@@ -1,9 +1,22 @@
 import '../../styles/screens/templates/add.css';
+import { OutlineButton } from '../../components/OutlineButton.tsx';
+import { ArrowLeft } from 'lucide-react';
+import { PageTitle } from '../../components/PageTitle.tsx';
+import { useNavigate } from 'react-router';
 
 export function Add() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
   return (
     <div id="screen-template-add">
-      template add
+      <header>
+        <OutlineButton label="Voltar" Icon={ArrowLeft} onClick={goBack}/>
+        <PageTitle>Novo template</PageTitle>
+      </header>
     </div>
   );
 }
