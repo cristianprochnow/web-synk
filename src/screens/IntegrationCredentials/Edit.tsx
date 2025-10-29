b7b44ea47ba5fd5cc7c8228d41edddb8fab7038a
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-28 21:34:34 -0300

    feat: aadd initial layout to edit credentials

---
+++ src/screens/IntegrationCredentials/Edit.tsx
@@ -0,0 +1,9 @@
+import '../../styles/screens/integration_credentials/edit.css';
+
+export function Edit() {
+  return (
+    <div id="screen-int-credentials-edit">
+      adicionar
+    </div>
+  );
+}
