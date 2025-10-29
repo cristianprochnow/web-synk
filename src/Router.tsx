28c34ccf454ab5a9606c960f518f5ceea468fc6d
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-28 21:35:08 -0300

    feat: add routes for integration credentials

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router'
 import { Dashboard } from './screens/Dashboard'
 import { Index } from './screens/Index'
+import { Add as IntegrationCredentialsAdd } from './screens/IntegrationCredentials/Add'
+import { Edit as IntegrationCredentialsEdit } from './screens/IntegrationCredentials/Edit'
+import { List as IntegrationCredentialsList } from './screens/IntegrationCredentials/List'
 import { NotFound } from './screens/NotFound'
 import { Add as PostAdd } from './screens/Posts/Add'
 import { Edit as PostEdit } from './screens/Posts/Edit'
@@ -11,21 +14,26 @@ import { List as TemplateList } from './screens/Templates/List'
 
 export function Router() {
     return (
-        <Routes>
-            <Route path="/" errorElement={<NotFound />} element={<Index />}>
-                <Route index element={<Dashboard />} />
-                <Route path="templates">
-                    <Route index element={<TemplateList />} />
-                    <Route path="add" element={<TemplateAdd />} />
-                    <Route path="edit/:template_id" element={<TemplateEdit />} />
-                </Route>
-                <Route path="posts">
-                    <Route index element={<PostList />} />
-                    <Route path="add" element={<PostAdd />} />
-                    <Route path="edit/:post_id" element={<PostEdit />} />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" errorElement={<NotFound />} element={<Index />}>
+          <Route index element={<Dashboard />} />
+          <Route path="templates">
+            <Route index element={<TemplateList />} />
+            <Route path="add" element={<TemplateAdd />} />
+            <Route path="edit/:template_id" element={<TemplateEdit />} />
+          </Route>
+          <Route path="posts">
+            <Route index element={<PostList />} />
+            <Route path="add" element={<PostAdd />} />
+            <Route path="edit/:post_id" element={<PostEdit />} />
+          </Route>
+          <Route path="integrations">
+            <Route index element={<IntegrationCredentialsList />} />
+            <Route path="add" element={<IntegrationCredentialsAdd />} />
+            <Route path="edit/:int_credential_id" element={<IntegrationCredentialsEdit />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     )
 }
