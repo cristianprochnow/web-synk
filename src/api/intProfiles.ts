37d88959de519cc4d450eed0ffd7b6a882cbc5c7
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-31 00:30:27 -0300

    feat: add request to list integration profiles

--- src/api/intProfiles.ts
+++ src/api/intProfiles.ts
@@ -1,4 +1,5 @@
 import { API_ENDPOINT } from './config';
+import type { IntCredentialsList } from './intCredentials';
 
 export type IntProfileOption = {
   int_profile_id: number
@@ -6,20 +7,38 @@ export type IntProfileOption = {
   color_hex: string
 };
 
-export type FetchIntProfileListResponse = {
+export type IntProfileItem = {
+  int_profile_id: number
+  int_profile_name: string
+  color_hex: string
+  color_name: string
+  credentials: IntCredentialsList[]
+};
+
+export type FetchIntProfileListResponse<T> = {
   resource: {
     ok: boolean
     error: string
   }
-  int_profiles: IntProfileOption[]
+  int_profiles: T[]
 };
 
-export async function fetchBasicIntProfiles(): Promise<FetchIntProfileListResponse> {
+export async function fetchBasicIntProfiles(): Promise<FetchIntProfileListResponse<IntProfileOption>> {
   const response = await fetch(API_ENDPOINT + '/int_profiles/basic');
 
-  return await response.json() as FetchIntProfileListResponse;
+  return await response.json() as FetchIntProfileListResponse<IntProfileOption>;
 }
 
-export function hasBasicIntProfiles(data: FetchIntProfileListResponse): boolean {
+export function hasBasicIntProfiles(data: FetchIntProfileListResponse<IntProfileOption>): boolean {
+  return data.int_profiles && data.int_profiles.length > 0;
+}
+
+export async function fetchIntProfiles(): Promise<FetchIntProfileListResponse<IntProfileItem>> {
+  const response = await fetch(API_ENDPOINT + '/int_profiles');
+
+  return await response.json() as FetchIntProfileListResponse<IntProfileItem>;
+}
+
+export function hasProfiles(data: FetchIntProfileListResponse<IntProfileItem>): boolean {
   return data.int_profiles && data.int_profiles.length > 0;
 }
