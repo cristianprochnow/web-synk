8e79fa357a81616943f4a9b9f9c076963620edae
Cristian Prochnow <cristianprochnow@gmail.com>
2025-10-31 21:00:41 -0300

    feat: add methods to request integration profiles

--- src/api/intProfiles.ts
+++ src/api/intProfiles.ts
@@ -23,6 +23,32 @@ export type FetchIntProfileListResponse<T> = {
   int_profiles: T[]
 };
 
+export type NewIntProfileData = {
+  int_profile_name: string | null;
+  color_id: number | null;
+  credentials: number[];
+};
+
+export type UpdateIntProfileData = {
+  int_profile_id: number;
+} & NewIntProfileData;
+
+export type IntProfileResponse<T> = {
+  resource: {
+    ok: boolean
+    error: string
+  }
+  int_profile: T
+};
+
+export type NewIntProfileResponse = {
+  int_profile_id: number;
+};
+
+export type UpdateIntProfileResponse = {
+  rows_affected: number;
+};
+
 export async function fetchBasicIntProfiles(): Promise<FetchIntProfileListResponse<IntProfileOption>> {
   const response = await fetch(API_ENDPOINT + '/int_profiles/basic');
 
@@ -41,4 +67,40 @@ export async function fetchIntProfiles(): Promise<FetchIntProfileListResponse<In
 
 export function hasProfiles(data: FetchIntProfileListResponse<IntProfileItem>): boolean {
   return data.int_profiles && data.int_profiles.length > 0;
+}
+
+export async function addIntProfile(template: NewIntProfileData): Promise<IntProfileResponse<NewIntProfileResponse>> {
+  const response = await fetch(API_ENDPOINT + '/int_profiles', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(template)
+  });
+
+  return await response.json() as IntProfileResponse<NewIntProfileResponse>;
+}
+
+export async function editIntProfile(post: UpdateIntProfileData): Promise<IntProfileResponse<UpdateIntProfileResponse>> {
+  const response = await fetch(API_ENDPOINT + '/int_profiles', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  });
+
+  return await response.json() as IntProfileResponse<UpdateIntProfileResponse>;
+}
+
+export async function deleteIntProfile(intProfileId: number): Promise<IntProfileResponse<UpdateIntProfileResponse>> {
+  const response = await fetch(API_ENDPOINT + '/int_profiles', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ int_profile_id: intProfileId })
+  });
+
+  return await response.json() as IntProfileResponse<UpdateIntProfileResponse>;
 }
