b1715b46cb18afc529d8a77d7cdba1045b84a21e
Cristian Prochnow <cristianprochnow@gmail.com>
2025-11-15 21:29:09 -0300

    feat: add login logic into Login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,15 +1,51 @@
 import { Eye, EyeClosed, LogIn } from 'lucide-react';
-import { useState } from 'react';
-import { Link } from 'react-router';
+import { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { login, type LoginRequestData } from '../api/users';
 import { ActionButton } from '../components/ActionButton';
 import { Input } from '../components/FieldGroup';
+import { useAuth } from '../contexts/Auth';
 import '../styles/screens/login.css';
 
 export function Login() {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
   const [isPassVisible, setPassVisible] = useState(false);
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passRef = useRef<HTMLInputElement>(null);
 
   function onPassVisible() {
     setPassVisible(!isPassVisible);
+  }
+
+  async function onHandlerLogin() {
+    const loginData: LoginRequestData = {
+      user_email: emailRef.current?.value || null,
+      user_pass: passRef.current?.value || null,
+    };
+
+    const data = await login(loginData);
+
+    if (!data.resource.ok) {
+      toast.error('Erro no login: ' + data.resource.error);
+
+      return;
+    }
+
+    auth.logIn(data.user.token, {
+      email: data.user.user_email,
+      id: data.user.user_id,
+      name: data.user.user_name
+    });
+
+    toast.success('Login realizado com sucesso! Redirecionando...');
+
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
   }
 
   return (
@@ -20,16 +56,16 @@ export function Login() {
         </header>
 
         <div className="fields">
-          <Input label="E-mail" alias="user_email" type="email" />
+          <Input label="E-mail" alias="user_email" type="email" ref={emailRef} />
 
           <div className="field-login">
-            <Input label="Senha" alias="user_pass" type={isPassVisible ? 'text' : 'password'} />
+            <Input label="Senha" alias="user_pass" type={isPassVisible ? 'text' : 'password'} ref={passRef} />
             <span onClick={onPassVisible}>
               {isPassVisible ? <Eye /> : <EyeClosed />}
             </span>
           </div>
 
-          <ActionButton label="Enviar" Icon={LogIn} />
+          <ActionButton label="Enviar" Icon={LogIn} onClick={onHandlerLogin} />
         </div>
 
         <footer>
